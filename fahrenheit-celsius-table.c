f56c14eb77168c90c88c7ae8bffa9e9e9759d4f0
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-05-03 20:55:20 +0700

    solve exercise

--- fahrenheit-celsius-table.c
+++ fahrenheit-celsius-table.c
@@ -11,9 +11,10 @@ main()
     step = 20;      // step size
 
     fahr = lower;
+    printf("F\tC\n");
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
+        printf("%.0f\t%.1f\n", fahr, celsius);
         fahr += step;
     }
 }
