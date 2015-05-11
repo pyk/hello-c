14026e2e8bb6c9ab308fd634f2317cf7ed2806a2
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-05-11 10:05:40 +0700

    Exercise 1-11: completed

---
+++ exercise-1-11.c
@@ -0,0 +1,5 @@
+// Exercise 1-11
+// How would you test the word count program? What kinds of input are most
+// likely to uncover bugs if there are any?
+
+// a new line should not be counted as new character.
