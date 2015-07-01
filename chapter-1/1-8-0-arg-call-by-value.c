0d467cdad278a6bfe8e4f4109450fb1a1ae9a6b1
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-07-01 11:56:26 +0700

    add: section 1.8

---
+++ chapter-1/1-8-0-arg-call-by-value.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int power(int base, int n);
+
+main() {
+    printf("4^6 = %d\n", power(4,6));
+}
+
+/* power: raise base to n-th power; n >= 0; version 2 */
+int power(int base, int n) {
+    int p;
+    for(p = 1; n > 0; n--)
+        p = p * base;
+    return p;
+}
